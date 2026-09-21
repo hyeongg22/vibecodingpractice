e11e7bc61f45a0ec3d77018406695e1a909a652e
hyeongg22 <hyeongg22@gmail.com>
2026-09-21 14:54:34 +0900

    배포 버전을 반영합니다: src/components/SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -15,7 +15,7 @@ function SearchBar({ searchText, onSearchTextChange, onSearch }) {
           type="search"
           value={searchText}
           onChange={(event) => onSearchTextChange(event.target.value)}
-          placeholder="예: 사랑, 용기, 시간, 믿음"
+          placeholder="예: 시간, time, 時間, tiempo"
           autoComplete="off"
         />
         <button type="submit" className="search-button">
